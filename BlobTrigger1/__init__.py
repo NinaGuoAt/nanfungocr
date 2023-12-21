94b2a8ea4455d58baf2aca85df27db02c581e884
NinaGuoAt <148028845+NinaGuoAt@users.noreply.github.com>
2023-12-21 19:24:26 +0800

    Add files via upload

--- BlobTrigger1/__init__.py
+++ BlobTrigger1/__init__.py
@@ -1,5 +1,5 @@
 import logging
-from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient, ContentSettings
 import azure.functions as func
 import json
 import time
@@ -13,7 +13,6 @@ import pandas as pd
 # This part is automatically generated
 def main(myblob: func.InputStream, outputDocument: func.Out[func.Document]):
 # def main(myblob: func.InputStream, outputDocument):
-# encoding = UTF"8"
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes")
@@ -24,7 +23,8 @@ def main(myblob: func.InputStream, outputDocument: func.Out[func.Document]):
     storage_account_name = "openaitestdata"
     storage_account_key = "ONYG9hw5vN4iqmsQWQ3bPF1MKX0SOghFZ7JstrbBD/8+XDduYLawrsPJvwNkKU7PhC4S+RgjqB33+AStuMN7Iw=="
         
-    model_id = "modelnf"
+    model_id = "modelnf2"
+    # model_id = "modelnf"
     post_url = endpoint + f"/formrecognizer/documentModels/{model_id}:analyze?api-version=2023-07-31"
     # Ref: https://westus2.dev.cognitive.microsoft.com/docs/services/form-recognizer-api-2023-07-31/operations/GetAnalyzeDocumentResult
     headers = {
@@ -42,7 +42,7 @@ def main(myblob: func.InputStream, outputDocument: func.Out[func.Document]):
     print("POST analyze succeeded:\n%s" % resp.headers)
     get_url = resp.headers["operation-location"]
 
-    wait_sec = 30
+    wait_sec = 10
     logging.info(f"waiting {wait_sec} seconds ...")
     time.sleep(wait_sec)
     logging.info(f"finish waiting ...")
@@ -50,7 +50,10 @@ def main(myblob: func.InputStream, outputDocument: func.Out[func.Document]):
 
     text1 = os.path.basename(myblob.name)
     resp = requests.get(url=get_url, headers={"Ocp-Apim-Subscription-Key": apim_key})
-    resp_json = json.loads(resp.text)
+    decoded_content = resp.text.encode('cp950', errors='ignore').decode('cp950')
+    # resp_json = json.loads(resp.text.encode(encoding = 'utf-8', errors='ignore'))
+    resp_json = json.loads(decoded_content)
+    logging.info(resp_json)
     status = resp_json["status"]
     logging.info(f"response status: {status}")
 
@@ -65,9 +68,9 @@ def main(myblob: func.InputStream, outputDocument: func.Out[func.Document]):
     results = resp_json
 
     # This is the connection to the blob storage, with the Azure Python SDK
-    output_container_name = "output"
-    blob_service_client = BlobServiceClient.from_connection_string(f'DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={storage_account_key};EndpointSuffix=core.windows.net')
-    container_client = blob_service_client.get_container_client(output_container_name)
+    # output_container_name = "output"
+    # blob_service_client = BlobServiceClient.from_connection_string(f'DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={storage_account_key};EndpointSuffix=core.windows.net')
+    # container_client = blob_service_client.get_container_client(output_container_name)
     
     # Parses the returned Document Intelligence response, constructs a .csv file, and uploads it to the output container
     key_value_dict = results['analyzeResult']['documents'][0]['fields']
@@ -81,18 +84,19 @@ def main(myblob: func.InputStream, outputDocument: func.Out[func.Document]):
         "Email":key_value_dict.get("Email", {}).get("valueString", "Not Applicable"),
         "VendorName":key_value_dict.get("VendorName", {}).get("valueString", "Not Applicable"),
         "VendorAddress":key_value_dict.get("VendorAddress", {}).get("valueString", "Not Applicable"),
-        "InvoiceId":key_value_dict.get("InvoiceId", {}).get("valueString", "Not Applicable")
+        "InvoiceId":key_value_dict.get("InvoiceId", {}).get("valueString", "Not Applicable"),
+        "URL": "https://openaitestdata.blob.core.windows.net/"+myblob.name
         }
         
     print(cleaned_key_value_dict)
     df = pd.DataFrame([cleaned_key_value_dict])
     
     # Here is the upload to the blob storage
-    tab1_csv = df.to_csv(header=True, index=False, mode='w')
-    name1 = (os.path.splitext(text1)[0]) +'.csv'
+    # tab1_csv = df.to_csv(header=True, index=False, mode='w')
+    # name1 = (os.path.splitext(text1)[0]) +'.csv'
 
     # Upload the results to Azure      
-    container_client.upload_blob(name=name1, data=tab1_csv)
+    # container_client.upload_blob(name=name1, data=tab1_csv)
     # outputDocument.set(func.Document.from_dict({"id": name1}))
     outputDocument.set(func.Document.from_dict(cleaned_key_value_dict))
     logging.info("Finished !!!")
